c3ddac86f58fd54f2226826000ed0e52ef57cc79
hhjyhxw <512162086@qq.com>
2022-11-27 16:14:46 +0800

    add

---
+++ java_io/src/main/java/com/icloud/file/User.java
@@ -0,0 +1,13 @@
+package com.icloud.file;
+
+public class User {
+    public int a = 20;
+    private int c = 10;
+
+    public User(){
+
+    }
+    public User(int a){
+        this.a = a;
+    }
+}
---
+++ java_io/src/main/java/com/icloud/file/UserTest.java
@@ -0,0 +1,8 @@
+package com.icloud.file;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User();
+        System.err.println(user.a);
+    }
+}
